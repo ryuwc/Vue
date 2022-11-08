d34a16a896b9d54edd4dc1a4d5568c07bc025271
ryuwc <wom05169@gmail.com>
2022-11-08 19:36:08 +0900

    라우터 활용 실습 하는 중

---
+++ day 6/problems/problem6-1/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="grid grid-cols-3 gap-1">
+      <nav class="flex flex-col col-span-1 mx-1 my-4 border-2 border-black border-solid rounded-lg h-72">
+        <router-link to="/">모든</router-link>
+        <router-link to="/about">오늘</router-link>
+        <router-link to="/">중요</router-link>
+      </nav>
+      <div class="col-span-2 my-4 border-2 border-black border-solid rounded-lg h-72">
+
+      </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+  text-decoration: none;
+
+}
+</style>
