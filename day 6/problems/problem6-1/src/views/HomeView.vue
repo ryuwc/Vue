177c3d81577811e59a5d4ad1cf09d86014722406
ryuwc <wom05169@gmail.com>
2022-11-09 19:40:44 +0900

    뷰 라우터를 활용한 todolist 틀만 잡아놓기

--- day 6/problems/problem6-1/src/views/HomeView.vue
+++ day 6/problems/problem6-1/src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="grid grid-cols-3 gap-1">
-      <nav class="flex flex-col col-span-1 mx-1 my-4 border-2 border-black border-solid rounded-lg h-72">
-        <router-link to="/">모든</router-link>
-        <router-link to="/about">오늘</router-link>
-        <router-link to="/">중요</router-link>
+      <nav class="flex flex-col col-span-1 mx-1 my-4 text-2xl border-2 border-black border-solid rounded-lg h-72">
+        <router-link to="/" class="py-1 pt-8">모든</router-link>
+        <router-link to="/about" class="py-1">오늘</router-link>
+        <router-link to="/important" class="py-1">중요</router-link>
       </nav>
       <div class="col-span-2 my-4 border-2 border-black border-solid rounded-lg h-72">
-
+        <h1>모든 할일</h1>
       </div>
     </div>
 </template>
@@ -21,7 +21,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
 nav a {
   font-weight: bold;
