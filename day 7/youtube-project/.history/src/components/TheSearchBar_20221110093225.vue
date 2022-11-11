859492e5d1de14622da60c51d78c0ed49f21b060
ryuwc <wom05169@gmail.com>
2022-11-11 18:48:31 +0900

    update

---
+++ day 7/youtube-project/.history/src/components/TheSearchBar_20221110093225.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" v-model="query" @keyup.enter="inputChange" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheSearchBar",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    inputChange(event) {
+      // console.log(event.target.value, this.query);
+      this.$emit('input-change', event.target.value)
+    },
+  },
+};
+</script>
+
+<style></style>
